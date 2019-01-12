b189bda6e03f5a40ce3e3a82574dd15b4d882634
Unknown <parsa2000kurd@gmail.com>
2019-01-11 19:30:39 -0800

    new grip profile

--- DeepVision/grip/grip.py
+++ DeepVision/grip/grip.py
@@ -28,7 +28,7 @@ class GripPipeline:
         self.convex_hulls_output = None
 
         self.__filter_contours_contours = self.convex_hulls_output
-        self.__filter_contours_min_area = 40.0
+        self.__filter_contours_min_area = 50.0
         self.__filter_contours_min_perimeter = 0.0
         self.__filter_contours_min_width = 0.0
         self.__filter_contours_max_width = 1000.0
