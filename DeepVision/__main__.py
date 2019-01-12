41c543fdb1db75594d7cf56094cecf4597cabe32
Carter <Retrax259@gmail.com>
2019-01-12 14:02:44 -0500

    Added commented code

--- DeepVision/__main__.py
+++ DeepVision/__main__.py
@@ -9,21 +9,31 @@ import cv2
 from grip import grip as grip
 from scipy.interpolate import interp1d
 
+
 if len(sys.argv) == 2:
 	roborio_address = sys.argv[1]
 else:
 	roborio_address = "roborio-5024-frc.local"
 
+#interpolate 1d from scipy
 m = interp1d([300, 600], [0,1])
 m2 = interp1d([0,299], [-1,0])
+
+
 # check for roborio
 print("Checking for RoboRIO")
-"""try:
-	requests.get("http://" + roborio_address + ":1181")
-	print("Found!")
-except:
-	print("FATAL! Roborio not found or cameraserver disabled!")
-	exit(1)"""
+
+
+#try:
+#	requests.get("http://" + roborio_address + ":1181")	
+#	print("Found!")
+#except:
+#	print("FATAL! Roborio not found or cameraserver disabled!")
+#
+#
+#	exit(1)
+
+
 
 # Init nt
 nt.init(roborio_address)
@@ -79,7 +89,10 @@ while True:
 			continue
 	centre = (x1 + x2)/2
 	
+	#Distance between camera center and hatch center
 	displacement = cameraWidth/2 - centre
+
+	#The angle betwen the center of the robot and hatch center
 	angle = displacement/degPerPixel
 	
 	distance = (max(x1,x2) - min(x1,x2))
